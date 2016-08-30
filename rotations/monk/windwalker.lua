af20b82a19e75706aeedd8f9111b6512644aa478
modhunter <pemodhunter@gmail.com>
2016-08-30 18:40:36 -0400

    why does interrupts and using a keybind cause the engine to pause for 6-10 seconds?

--- rotations/monk/windwalker.lua
+++ rotations/monk/windwalker.lua
@@ -93,7 +93,7 @@ local _OOC = {
 	{ "Effuse", { "player.health < 90", "player.lastmoved >= 1", "!player.combat" }, "player" },
 
 	-- Automatic res of dead party members
-	{ "@NOC.resDeadFriends('Resuscitate')", (function() return F('auto_res') end) },
+	--{ "@NOC.resDeadFriends('Resuscitate')", (function() return F('auto_res') end) },
 
 	-- TODO: Add support for (optional) automatic potion use w/pull timer
 }
@@ -106,7 +106,7 @@ local _All = {
 	{ "/stopcasting\n/stopattack\n/cleartarget\n/stopattack\n/cleartarget\n/nep mt", { "player.time >= 300", (function() return F('dpstest') end) }},
 
 	-- Cancel CJL when we're in melee range and having cast at least 1 second (delta < 2)- to help with controlling Hit Combo stuff.
-	{"!/stopcasting", { "target.range <= 5", "player.casting(Crackling Jade Lightning)", "player.casting.delta < 2" }},
+	--{"!/stopcasting", { "target.range <= 5", "player.casting(Crackling Jade Lightning)", "player.casting.delta < 2" }},
 
 	-- FREEDOOM!
 	{ "116841", 'player.state.disorient' }, -- Tiger's Lust = 116841
@@ -116,25 +116,26 @@ local _All = {
 }
 
 local _Cooldowns = {
-	{ "Touch of Death", "!player.spell.usable(Gale Burst)" },
-	{ "Touch of Death", { "player.spell.usable(Gale Burst)", "player.spell(Strike of the Windlord).cooldown <= 8", "player.spell(Fists of Fury).cooldown <= 3", "player.spell(Rising Sun Kick).cooldown < 8" }},
+	{ "Touch of Death" },
+	--{ "Touch of Death", "!player.spell.usable(Gale Burst)" },
+	--{ "Touch of Death", { "player.spell.usable(Gale Burst)", "player.spell(Strike of the Windlord).cooldown <= 8", "player.spell(Fists of Fury).cooldown <= 3", "player.spell(Rising Sun Kick).cooldown < 8" }},
 	{ "Lifeblood" },
 	{ "Berserking" },
 	{ "Blood Fury" },
 	-- Use Xuen only while hero or potion is active
 	{ "Invoke Xuen, the White Tiger", "player.hashero" },
 	{ "Invoke Xuen, the White Tiger", "player.buff(156423)" }, -- Draenic Agility Potion (WoD)
-	{ "Invoke Xuen, the White Tiger", "player.buff(188027)" }, -- Potion of Deadly Grace (Legion)
+	--{ "Invoke Xuen, the White Tiger", "player.buff(188027)" }, -- Potion of Deadly Grace (Legion)
 }
 
 local _Survival = {
 	{ "Effuse", { "player.energy >= 60", "player.lastmoved >= 0.5", effuse }, "player" },
-	{ "Healing Elixir", { "player.spell(Healing Elixir).charges >= 2", "or", { "player.spell(Healing Elixir).charges = 1", "player.spell(Healing Elixir).cooldown < 3" }, "!lastcast(Healing Elixir)", HealingElixir }, "player" },
+	--{ "Healing Elixir", { "player.spell(Healing Elixir).charges >= 2", "or", { "player.spell(Healing Elixir).charges = 1", "player.spell(Healing Elixir).cooldown < 3" }, "!lastcast(Healing Elixir)", HealingElixir }, "player" },
 
 	-- TODO: Update for legion's equivillant to healing tonic 109223
 	{ "#109223", healthstn, "player" }, -- Healing Tonic
 	{ '#5512', healthstn, "player" }, -- Healthstone
-	{ "Detox", "player.dispellable(Detox)", "player" },
+	--{ "Detox", "player.dispellable(Detox)", "player" },
 }
 
 local _Interrupts = {
@@ -189,7 +190,7 @@ local _Openner = {
 
 local _AoE = {
 	{ 'Spinning Crane Kick', { '!lastcast(Spinning Crane Kick)', goodLastCast, "player.spell(Spinning Crane Kick).count >= 2" }},
-	{ "@NOC.AoEMissingDebuff('Rising Sun Kick', 'Mark of the Crane', 5)", (function() return F('auto_dot') end) },
+	--{ "@NOC.AoEMissingDebuff('Rising Sun Kick', 'Mark of the Crane', 5)", (function() return F('auto_dot') end) },
 	{ "Rising Sun Kick" },
 	{ "Rushing Jade Wind", { "player.chi >= 1", "!lastcast(Rushing Jade Wind)", goodLastCast }},
 	{ 'Spinning Crane Kick', { '!lastcast(Spinning Crane Kick)', goodLastCast, "player.spell(Spinning Crane Kick).count >= 4" }},
@@ -198,22 +199,22 @@ local _AoE = {
 		{ "Chi Burst", "!player.moving" },
 	}, { "!player.buff(Serenity)", "player.timetomax > 2" }},
 	{{
-		{ "@NOC.AoEMissingDebuff('Blackout Kick', 'Mark of the Crane', 5)", { "player.buff(Blackout Kick!)", (function() return F('auto_dot') end) }},
+		--{ "@NOC.AoEMissingDebuff('Blackout Kick', 'Mark of the Crane', 5)", { "player.buff(Blackout Kick!)", (function() return F('auto_dot') end) }},
 		{ "Blackout Kick", "player.buff(Blackout Kick!)" },
-  	{ "@NOC.AoEMissingDebuff('Blackout Kick', 'Mark of the Crane', 5)", { "player.chi > 1", (function() return F('auto_dot') end) }},
+  	--{ "@NOC.AoEMissingDebuff('Blackout Kick', 'Mark of the Crane', 5)", { "player.chi > 1", (function() return F('auto_dot') end) }},
 		{ "Blackout Kick", "player.chi > 1" },
 	}, { "!lastcast(Blackout Kick)", goodLastCast }},
 
-	{ "@NOC.AoEMissingDebuff('Tiger Palm', 'Mark of the Crane', 5)", { "!player.buff(Serenity)", "player.chidiff > 1", (function() return F('auto_dot') end), "!lastcast(Tiger Palm)", goodLastCast }},
+	--{ "@NOC.AoEMissingDebuff('Tiger Palm', 'Mark of the Crane', 5)", { "!player.buff(Serenity)", "player.chidiff > 1", (function() return F('auto_dot') end), "!lastcast(Tiger Palm)", goodLastCast }},
 	{ "Tiger Palm", { "!player.buff(Serenity)", "player.chidiff > 1", "!lastcast(Tiger Palm)", goodLastCast }},
 
 }
 
 local _ST = {
-	{ 'Spinning Crane Kick', { (function() return F('smart_rjw') end), '!lastcast(Spinning Crane Kick)', goodLastCast, { "player.spell(Spinning Crane Kick).count >= 6", "or", { "player.spell(Spinning Crane Kick).count >= 2", "player.area(8).enemies >= 2" }}}},
+	--{ 'Spinning Crane Kick', { (function() return F('smart_rjw') end), '!lastcast(Spinning Crane Kick)', goodLastCast, { "player.spell(Spinning Crane Kick).count >= 6", "or", { "player.spell(Spinning Crane Kick).count >= 2", "player.area(8).enemies >= 2" }}}},
 	{ "Rising Sun Kick" },
 	{ "Rushing Jade Wind", { "player.chi > 1", "!lastcast(Rushing Jade Wind)", goodLastCast }},
-	{ 'Spinning Crane Kick', { (function() return F('smart_rjw') end), '!lastcast(Spinning Crane Kick)', goodLastCast, { "player.spell(Spinning Crane Kick).count >= 4", "or", "player.area(8).enemies >= 2" }}},
+	--{ 'Spinning Crane Kick', { (function() return F('smart_rjw') end), '!lastcast(Spinning Crane Kick)', goodLastCast, { "player.spell(Spinning Crane Kick).count >= 4", "or", "player.area(8).enemies >= 2" }}},
 	{{
 		{ "Chi Wave" }, -- 40 yard range 0 energy, 0 chi
 		{ "Chi Burst", "!player.moving" },
@@ -228,12 +229,12 @@ local _ST = {
 local _Melee = {
 	{ 'Serenity', { "player.spell(Strike of the Windlord).cooldown <= 8", "player.spell(Rising Sun Kick).cooldown < 8", "player.spell(Fists of Fury).cooldown <= 3" }},
 	{ 'Serenity', { "player.spell(Rising Sun Kick).cooldown < 8", "player.spell(Fists of Fury).cooldown <= 3" }},
-	{ "Energizing Elixir", { "player.energydiff > 0", "player.chi <= 1", "!player.buff(Serenity)" }},
+	--{ "Energizing Elixir", { "player.energydiff > 0", "player.chi <= 1", "!player.buff(Serenity)" }},
 	{ "Rushing Jade Wind", { "player.buff(Serenity)", "!lastcast(Rushing Jade Wind)", goodLastCast }},
 	{ "Strike of the Windlord" },
-	{ 'Spinning Crane Kick', { (function() return F('smart_rjw') end), '!lastcast(Spinning Crane Kick)', goodLastCast, { "player.spell(Spinning Crane Kick).count >= 17" }}},
+	--{ 'Spinning Crane Kick', { (function() return F('smart_rjw') end), '!lastcast(Spinning Crane Kick)', goodLastCast, { "player.spell(Spinning Crane Kick).count >= 17" }}},
 	{ "Whirling Dragon Punch" },
-	{ 'Spinning Crane Kick', { (function() return F('smart_rjw') end), '!lastcast(Spinning Crane Kick)', goodLastCast, { "player.spell(Spinning Crane Kick).count >= 12" }}},
+	--{ 'Spinning Crane Kick', { (function() return F('smart_rjw') end), '!lastcast(Spinning Crane Kick)', goodLastCast, { "player.spell(Spinning Crane Kick).count >= 12" }}},
 	{ "Fists of Fury" },
 
 	{ _AoE, { 'player.area(8).enemies >= 3', 'modifier.multitarget' }},
@@ -248,20 +249,20 @@ local _Melee = {
 	}, "!player.buff(Hit Combo)" },
 
 	-- CJL when we're using Hit Combo as a last resort, and perhaps with other constraints like "GoodLastCast"
-	{ "Crackling Jade Lightning", { "!lastcast(Crackling Jade Lightning)", goodLastCast }},
+	--{ "Crackling Jade Lightning", { "!lastcast(Crackling Jade Lightning)", goodLastCast }},
 
 	--{ (function() print('I have nothing to do ('..GetTime()..')'); end) },
 }
 
 NeP.Engine.registerRotation(269, '[|cff'..NeP.Interface.addonColor..'NoC|r] Monk - Windwalker',
 	{ -- In-Combat
-		--{'pause', 'modifier.shift'},
+		{'pause', 'modifier.shift'},
 		{_All},
 		{_Survival, 'player.health < 100'},
-		{_Interrupts, {'target.interruptAt(40)' }},
-		{_Cooldowns, 'modifier.cooldowns'},
+		{_Interrupts, { 'target.interruptAt(55)', 'target.inMelee' }},
+		{_Cooldowns, 'modifier.cooldowns' },
 		{_SEF, { "target.range <= 5", (function() return F('SEF') end) }},
-		{_Openner, { (function() return F('opener') end), "player.time < 10" }},
+		--{_Openner, { (function() return F('opener') end), "player.time < 10" }},
 		{_Melee, { "target.range <= 5" }},
 		{_Ranged, { "target.range > 8", "target.range <= 40" }},
 	}, _OOC, exeOnLoad)
