9b0ba5919efe6391e64a6fa1c670b61b699722a6
Jaime Marques <mrthesoulz@gmail.com>
2016-10-13 02:32:58 +0100

    get rid of some of these nets

--- rotations/demonhunter/havoc.lua
+++ rotations/demonhunter/havoc.lua
@@ -20,22 +20,22 @@ end
 local _All = {
 	-- keybind
 	--{ 'pause', 'keybind(shift)' },
-  { "Chaos Nova", "keybind(lcontrol)" },
-  --{ "Darkness", "keybind(lalt)" }, -- reserve alt for Metamorphosis instead
+	{ "Chaos Nova", "keybind(lcontrol)" },
+	--{ "Darkness", "keybind(lalt)" }, -- reserve alt for Metamorphosis instead
 
 	{ "/stopcasting\n/stopattack\n/cleartarget\n/stopattack\n/cleartarget", { "player.combat.time >= 300", (function() return F('dpstest') end) }},
 
 	-- Vengeful Retreat backwards through the target to minimize downtime.
-  --vengeful_retreat,if=(talent.prepared.enabled|talent.momentum.enabled)&buff.prepared.down&buff.momentum.down
-  -- Fel Rush for Momentum and for fury from Fel Mastery.
-  --fel_rush,animation_cancel=1,if=(talent.momentum.enabled|talent.fel_mastery.enabled)&(!talent.momentum.enabled|(charges=2|cooldown.vengeful_retreat.remains>4)&buff.momentum.down)&(!talent.fel_mastery.enabled|fury.deficit>=25)&raid_event.movement.in>charges*10
+	--vengeful_retreat,if=(talent.prepared.enabled|talent.momentum.enabled)&buff.prepared.down&buff.momentum.down
+	-- Fel Rush for Momentum and for fury from Fel Mastery.
+	--fel_rush,animation_cancel=1,if=(talent.momentum.enabled|talent.fel_mastery.enabled)&(!talent.momentum.enabled|(charges=2|cooldown.vengeful_retreat.remains>4)&buff.momentum.down)&(!talent.fel_mastery.enabled|fury.deficit>=25)&raid_event.movement.in>charges*10
 
 }
 
 local _Cooldowns = {
-  { "Lifeblood" },
-  { "Berserking" },
-  { "Blood Fury" },
+	{ "Lifeblood" },
+	{ "Berserking" },
+	{ "Blood Fury" },
 
 	{ "Metamorphosis", 'keybind(lalt)&{!player.buff(Metamorphosis)&!talent(7,3)||player.spell(Eye Beam).cooldown < 0.5&!talent(7,1)||player.spell(Chaos Blades).cooldown < 0.5&!talent(5,3)||target.debuff(Nemesis)||player.spell(Nemesis).cooldown < 0.5}', "mouseover.ground" },
 
@@ -44,9 +44,9 @@ local _Cooldowns = {
 }
 
 local _Survival = {
-  { "Blur", 'player.health <= UI(blur)'},
-  { "Desperate Instincts", 'player.health <= UI(desperate)'},
-  { "Netherwalk",  'player.health <= UI(netherwalk)' },
+	{ "Blur", 'player.health <= UI(blur)'},
+	{ "Desperate Instincts", 'player.health <= UI(desperate)'},
+	{ "Netherwalk",	'player.health <= UI(netherwalk)' },
 
 	{ "#109223", 'player.health <= UI(Healthstone)', "player" }, -- Healing Tonic
 	{ '#5512', 'player.health <= UI(Healthstone)', "player" }, -- Healthstone
@@ -72,68 +72,55 @@ local _Ranged = {
 
 local _Melee = {
 	-- Rotation
-  { "Fury of the Illidari" }, -- Fury of the Illidari
+	{ "Fury of the Illidari" }, -- Fury of the Illidari
 
 	-- TODO: figure out how to handle the wierd 'worth using' crap from the simc APL
 	-- TODO: implement true gcd checking instead of assuming 1.5s everywhere
-  { "Death Sweep" },
-  { "Demon's Bite", { "player.buff(Metamorphosis).duration > 1.5", "player.spell(Blade Dance).cooldown < 1.5", "player.fury < 70" }},
+	{ "Death Sweep" },
+	{ "Demon's Bite", "player.buff(Metamorphosis).duration > 1.5&player.spell(Blade Dance).cooldown < 1.5&player.fury < 70" },
 
-  { "Blade Dance" },
+	{ "Blade Dance" },
 
-  {{
-    { "Fel Barrage", "player.buff(Momentum)" },
-    { "Fel Barrage", "!talent(5,1)" },
-  }, { "player.spell(Fel Barrage).charges >= 5" }},
+	{ "Fel Barrage", "player.buff(Momentum)&player.spell(Fel Barrage).charges >= 5"  },
+	{ "Fel Barrage", "!talent(5,1)&player.spell(Fel Barrage).charges >= 5" },
+	{ "Throw Glaive", "{!talent(6,1)||!talent(5,1)||player.buff(Momentum)}&talent(3,3)&toggle(multitarget)&layer.area(10).enemies >= 2" },
+	{ "Fel Erruption" },
 
-  {{
-    { "Throw Glaive", "!talent(6,1)" },
-    { "Throw Glaive", "!talent(5,1)" },
-    { "Throw Glaive", "player.buff(Momentum)" },
-  }, { "talent(3,3)", "toggle(multitarget)", "player.area(10).enemies >= 2" }},
+	{ "Felblade", "player.furydiff >= 30&!player.buff(Prepared)"},
+	{ "Felblade", "player.furydiff >= 42&player.buff(Prepared)"},
 
-  { "Fel Erruption" },
+	{ "Annihilation", "!talent(5,1)" },
+	{ "Annihilation", "player.buff(Momentum)" },
+	{ "Annihilation", "player.furydiff >= 30&!player.buff(Prepared)"},
+	{ "Annihilation", "player.furydiff >= 42&player.buff(Prepared)" },
+	{ "Annihilation", "player.buff(Metamorphosis).duration < 2" },
+	{ "Throw Glaive", "{!talent(6,1)||!talent(5,1)||player.buff(Momentum)}&talent(3,3)" },
 
-  { "Felblade", { "player.furydiff >= 30", "!player.buff(Prepared)" }},
-  { "Felblade", { "player.furydiff >= 42", "player.buff(Prepared)" }},
-
-  { "Annihilation", "!talent(5,1)" },
-  { "Annihilation", "player.buff(Momentum)" },
-  { "Annihilation", { "player.furydiff >= 30", "!player.buff(Prepared)" }},
-  { "Annihilation", { "player.furydiff >= 42", "player.buff(Prepared)" }},
-  { "Annihilation", "player.buff(Metamorphosis).duration < 2" },
-
-  {{
-    { "Throw Glaive", "!talent(6,1)" },
-    { "Throw Glaive", "!talent(5,1)" },
-    { "Throw Glaive", "player.buff(Momentum)" },
-  }, { "talent(3,3)" }},
-
-	{ "Eye Beam", { "!talent(7,3)", { "toggle(multitarget)", "player.area(15).enemies >= 2", "or", "!player.buff(Metamorphosis)" }, { "toggle(multitarget)", "player.area(15).enemies >= 2", "or", "player.level = 100" }}},
+	{ "Eye Beam", "!talent(7,3){toggle(multitarget)&player.area(15).enemies >= 2||!player.buff(Metamorphosis)}&{toggle(multitarget)&player.area(15).enemies >= 2||player.level = 100}"},
 	--{ "Eye Beam", { "!talent(7,3)", "toggle(multitarget)", "player.area(15).enemies >= 2", }},
 	--{ "Eye Beam", { "!talent(7,3)", "!player.buff(Metamorphosis)", "player.level = 100" }},
 
-  {{
-    { "Demon's Bite", { "player.spell(Blade Dance).cooldown < 1.5", "player.fury < 55" }},
-    { "Demon's Bite", { "talent(7,3)", "player.spell(Eye Beam).cooldown < 1.5", "player.furydiff >= 20" }},
-    { "Demon's Bite", { "talent(7,3)", "player.spell(Eye Beam).cooldown < 3", "player.furydiff >= 45" }},
-  }, { "!player.buff(Metamorphosis)" }},
+	{{
+		{ "Demon's Bite", "player.spell(Blade Dance).cooldown < 1.5&player.fury < 55" },
+		{ "Demon's Bite", "talent(7,3)&player.spell(Eye Beam).cooldown < 1.5&player.furydiff >= 20" },
+		{ "Demon's Bite", "talent(7,3)&player.spell(Eye Beam).cooldown < 3&player.furydiff >= 45" },
+	}, "!player.buff(Metamorphosis)" },
 
-  { "Throw Glaive", { "!player.buff(Metamorphosis)", "toggle(multitarget)", "player.area(10).enemies >= 3" }},
+	{ "Throw Glaive", "!player.buff(Metamorphosis)&toggle(multitarget)&player.area(10).enemies >= 3" },
 
-  { "Chaos Strike", "!talent(5,1)" },
-  { "Chaos Strike", "player.buff(Momentum)" },
-  { "Chaos Strike", { "player.furydiff >= 30", "!player.buff(Prepared)" }},
-  { "Chaos Strike", { "player.furydiff >= 42", "player.buff(Prepared)" }},
+	{ "Chaos Strike", "!talent(5,1)" },
+	{ "Chaos Strike", "player.buff(Momentum)" },
+	{ "Chaos Strike", { "player.furydiff >= 30", "!player.buff(Prepared)" }},
+	{ "Chaos Strike", { "player.furydiff >= 42", "player.buff(Prepared)" }},
 
-  {{
-    { "Fel Barrage", "player.buff(Momentum)" },
-    { "Fel Barrage", "!talent(5,1)" },
-  }, { "player.spell(Fel Barrage).charges >= 4", "!player.buff(Metamorphosis)" }},
+	{{
+		{ "Fel Barrage", "player.buff(Momentum)" },
+		{ "Fel Barrage", "!talent(5,1)" },
+	}, { "player.spell(Fel Barrage).charges >= 4", "!player.buff(Metamorphosis)" }},
 
 	{ "Throw Glaive" },
 
-  { "Demon's Bite" },
+	{ "Demon's Bite" },
 }
 
 local _Rotation = {
