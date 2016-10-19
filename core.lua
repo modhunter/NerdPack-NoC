b5039d22825c0a27f470600aa335ffd9b592452e
modhunter <pemodhunter@gmail.com>
2016-10-18 23:33:00 -0400

    initial add of fakeunits stuff

--- core.lua
+++ core.lua
@@ -57,6 +57,24 @@ C_Timer.NewTicker(0.1, (function()
 	end
 end), nil)
 
+NeP.FakeUnits.Add('NOC_sck', function(num, debuff)
+	local tempTable = {}
+	for i=1, #NeP.OM.unitEnemie do
+		local Obj = NeP.OM.unitEnemie[i]
+		if (not NeP.DSL.Get('debuff')(Obj.key, debuff)) and (NeP.Engine.LineOfSight('player', Obj.key)) then
+		--if not NeP.DSL.Get('debuff')(Obj.key, debuff) then
+			tempTable[#tempTable+1] = {
+				key = Obj.key,
+				--prio = prio
+			}
+		end
+	end
+	if tempTable[num] then
+		--print("NOC_NoDebuff: returning: "..tempTable[num].key.." ("..time()..")");
+		return tempTable[num].key
+	end
+	--return false
+end)
 
 NeP.Library:Add('NOC', {
 
